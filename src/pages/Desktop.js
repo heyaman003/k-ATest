405af1bd291bfc24748fbc254e9f1131137b7a37
amankr003 <amankumar@teesta.co>
2024-02-06 14:43:19 +0530

    final code

---
+++ src/pages/Desktop.js
@@ -0,0 +1,165 @@
+import "./Desktop.css";
+
+const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="footer">
+        <div className="background" />
+        <div className="links">
+          <div className="privacy-policy">Privacy Policy</div>
+          <div className="privacy-policy">Terms of Service</div>
+        </div>
+        <div className="designed-by-ka">{`© 2024, Designed by k&a `}</div>
+      </div>
+      <div className="footer-links">
+        <div className="navigation">
+          <div className="product">Product</div>
+          <div className="use-cases">Use Cases</div>
+          <div className="use-cases">For Business</div>
+          <div className="use-cases">Resources</div>
+          <div className="use-cases">Pricing</div>
+        </div>
+        <div className="navigation">
+          <div className="product">Downloads</div>
+          <div className="use-cases">Desktop App</div>
+          <div className="use-cases">Chrome Extension</div>
+          <div className="use-cases">Safari Extension</div>
+          <div className="use-cases">iOS App</div>
+        </div>
+        <div className="navigation">
+          <div className="product">Resources</div>
+          <div className="use-cases">Blog</div>
+          <div className="use-cases">{`Help & Support`}</div>
+          <div className="use-cases">Community</div>
+          <div className="use-cases">Status</div>
+        </div>
+        <div className="navigation">
+          <div className="product">Company</div>
+          <div className="use-cases">About Us</div>
+          <div className="use-cases">Careers</div>
+          <div className="use-cases">Media Kit</div>
+          <div className="use-cases">Sitemap</div>
+        </div>
+      </div>
+      <div className="navigation4">
+        <div className="work">Work</div>
+        <div className="work">Expertise</div>
+        <div className="work">About</div>
+        <div className="work">Insight</div>
+      </div>
+      <div className="navbar">
+        <div className="navbar-child" />
+        <div className="start-a-project">Start a project</div>
+        <img className="envelope-icon" alt="" src="/envelope.svg" />
+      </div>
+      <img
+        className="hp-desktop-gradiant-1-icon"
+        alt=""
+        src="/hpdesktopgradiant-1@2x.png"
+      />
+      <header className="selling-point">
+        <div className="frame-parent">
+          <div className="frame-child" />
+          <div className="parent">
+            <div className="div">35,000</div>
+            <div className="websites-already-thriving">
+              Websites Already Thriving with Us!
+            </div>
+          </div>
+        </div>
+        <div className="experience-the-power-of-speed-parent">
+          <div className="experience-the-power-container">
+            <p className="experience">Experience</p>
+            <p className="the-power-of">the power of</p>
+            <p className="experience">
+              <span className="speed">speed</span>
+              <span> with our</span>
+            </p>
+            <p className="experience">cutting-edge</p>
+            <p className="hosting-infrastructure">hosting infrastructure</p>
+          </div>
+          <div className="component-1">
+            <div className="button1">Read our story</div>
+          </div>
+        </div>
+      </header>
+      <div className="social-proof">
+        <div className="text">
+          <p className="join-400">{`Join 400+ `}</p>
+          <p className="companies-already-growing">companies already growing</p>
+        </div>
+      </div>
+      <div className="component-2">
+        <div className="title">Creating</div>
+        <b className="title1">
+          Discerning Digital Works and Communication Collaterals
+        </b>
+      </div>
+      <div className="logos">
+        <div className="logos1">
+          <div className="puma-logo-parent">
+            <img className="puma-logo-icon" alt="" src="/pumalogo.svg" />
+            <img className="whatsapp-icon" alt="" src="/whatsapp.svg" />
+            <img
+              className="google-2015-1-icon"
+              alt=""
+              src="/google2015-1.svg"
+            />
+            <img className="spotify-1-1-icon" alt="" src="/spotify1-1.svg" />
+            <img className="microsoft-1-icon" alt="" src="/microsoft-1.svg" />
+            <img
+              className="fedex-express-6-1-icon"
+              alt=""
+              src="/fedexexpress6-1.svg"
+            />
+            <img className="airbnb-1-icon" alt="" src="/airbnb-1.svg" />
+          </div>
+          <div className="puma-logo-parent">
+            <div className="new-youtube-logo">
+              <img className="vector-icon" alt="" src="/vector.svg" />
+              <img className="group-icon" alt="" src="/group.svg" />
+            </div>
+            <img className="walmart-1-icon" alt="" src="/walmart-1.svg" />
+            <img className="binance-1-icon" alt="" src="/binance-1.svg" />
+            <img className="slack-icon" alt="" src="/slack.svg" />
+            <img className="canon-logo-icon" alt="" src="/canonlogo.svg" />
+            <img className="spacex-icon" alt="" src="/spacex.svg" />
+          </div>
+        </div>
+        <div className="overlay-left" />
+        <div className="overlay-right" />
+      </div>
+      <img className="icon" alt="" src="/1111-1@2x.png" />
+      <div className="we-are-creative-container">
+        <p className="we-are">{`We are `}</p>
+        <p className="creative-designers-insightful">
+          <b className="creative-designers-insightful1">
+            Creative Designers, Insightful Strategists, and Exceptional
+          <br />Engineers
+          </b>
+        </p>
+      </div>
+      <div className="the-organization-is">
+        The organization is focused on foregrounding expertise in its services,
+        staying updated on global trends, refining domain knowledge, and
+        adapting to evolving content creation arenas to effectively serve client
+        brands.
+      </div>
+      <img className="desktop-child" alt="" src="/vector-1.svg" />
+      
+      <div className="layer-1-parent">
+        <img className="layer-1-icon" alt="" src="/k&a.svg" /> 
+       
+        <img className="layer-1-icon1" alt="" src="/layer-1.svg" />
+        <b className="lets-unite-for">{`Let's unite for innovation `}</b>
+        <img
+          className="psd226-1-icon"
+          alt=""
+          src="/23126555-199-psd226-1@2x.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Desktop;
